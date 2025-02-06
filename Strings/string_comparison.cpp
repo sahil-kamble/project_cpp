ee9c1bb4bae9fd2344b7679470acbe2eff5c4da6
sahil-kamble <157009104+sahil-kamble@users.noreply.github.com>
2025-02-06 07:56:46 +0530

    Add files via upload

---
+++ Strings/string_comparison.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    // String comparison
+    string str1 = "apple";
+    string str2 = "orange";
+
+    if (str1 == str2) {
+        cout << "Strings are equal." <<endl;
+    } else {
+        cout << "Strings are not equal." << endl;
+    }
+
+    return 0;
+}
